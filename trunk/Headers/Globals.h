672dd1e499c9371da88ca8b92bc6d014b597a03a
Rainer Sieger <github@siegersoft.de>
2016-09-29 12:26:31 +0200

    Version 12.25

--- trunk/Headers/Globals.h
+++ trunk/Headers/Globals.h
@@ -19,7 +19,7 @@
     const QString _PROGRAMDOI_           = "doi:10.1594/PANGAEA.835398";
     const QString _PROGRAMREADME_        = "https://wiki.pangaea.de/wiki/Split2Events";
 
-    const QString _VERSION_              = "12.24";	// Version number, 2016-08-28
+    const QString _VERSION_              = "12.25";	// Version number, 2016-09-29
     const QString _PROVERSION_           = "Split2Event - Project - 2013-03-23";
 
     const int   _NOERROR_                = 0;
